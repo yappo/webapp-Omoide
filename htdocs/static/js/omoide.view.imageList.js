9a5f616d3f978b3ec796cc5cd79c079f3c5eb79b
Kazuhiro Osawa <yappo@shibuya.pl>
2011-07-02 19:20:53 +0900

    image centering

--- htdocs/static/js/omoide.view.imageList.js
+++ htdocs/static/js/omoide.view.imageList.js
@@ -22,10 +22,26 @@ window.Omoide.View.ImageList.prototype = {
 	appendImages: function(list) {
 		var html = "";
 		$.each(list, function(i, obj) {
-			html = html + '<div class="thumbnail"><img src="' + Omoide.createImageUrl("s", obj.id, true) + '" /></div>';
+			html = html + '<div id="img-' + obj.id + '" class="thumbnail"><img class="thumbnailImage" src="' + Omoide.createImageUrl("s", obj.id, true) + '" /></div>';
+		});
+		var $html = $(html);
+		$html.find(".thumbnailImage").load(function(e) {
+			var $this = $(this);
+			var paddingLeft = Math.floor((75 - $this.width()) / 2);
+			var paddingRight = 75 - paddingLeft - $this.width();
+			var paddingTop = Math.floor((75 - $this.height()) / 2);
+			var paddingBottom = 75 - paddingTop - $this.height();
+			$this.parent().css({
+				"width": $this.width()+"px",
+				"height": $this.height()+"px",
+				"padding-left": paddingLeft+"px",
+				"padding-right": paddingRight+"px",
+				"padding-top": paddingTop+"px",
+				"padding-bottom": paddingBottom+"px"
+			});
 		});
 		var $container = $(this.containerId);
-		$container.html(html);
+		$container.html($html);
 	},
 
 	fixupContainerSize: function(args) {
