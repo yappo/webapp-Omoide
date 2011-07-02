f72e5c68f0fa98ff5dbfa96e7628941e16b99e0a
Kazuhiro Osawa <yappo@shibuya.pl>
2011-07-02 18:27:05 +0900

    support window resize

--- htdocs/static/js/omoide.controller.mainContainer.js
+++ htdocs/static/js/omoide.controller.mainContainer.js
@@ -17,6 +17,11 @@ window.Omoide.Controller.MainContainer = function(args) {
 
 	this.fixupContainerSize();
 
+	var self = this;
+	$(window).resize(function() {
+		self.onResize();
+	});
+
 	return this;
 };
 
@@ -24,6 +29,18 @@ window.Omoide.Controller.MainContainer.prototype = {
 	albumListContainer: null,
 	imageListContainer: null,
 	containerId: null,
+	onResizeTimeoutId: null,
+
+	onResize: function() {
+		if (this.onResizeTimeoutId !== null) {
+			clearTimeout(this.onResizeTimeoutId);
+			this.onResizeTimeoutId = null;
+		}
+		var self = this;
+		this.onResizeTimeoutId = setTimeout(function() {
+			self.fixupContainerSize();
+		}, 50);
+	},
 
 	fixupContainerSize: function() {
 		var widthPadding = 4;
